9f8836940a7aa4e02325168ad07522b05fffb954
starryboram <boramplay@naver.com>
2024-10-19 17:57:10 +0900

    [study] Thread sleep util

---
+++ src/thread/util/ThreadUtils.java
@@ -0,0 +1,15 @@
+package thread.util;
+
+import static thread.util.MyLogger.log;
+
+public abstract class ThreadUtils {
+
+    public static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            log("Interrupt Occurred, " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
