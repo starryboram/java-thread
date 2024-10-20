209d3e7aebbb2fba1252e944c770b21ee93d407f
starryboram <boramplay@naver.com>
2024-10-20 13:45:35 +0900

    [study] Print Test(use thread)

---
+++ src/thread/control/printer/MyPrinter3.java
@@ -0,0 +1,57 @@
+package thread.control.printer;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import static thread.util.MyLogger.log;
+
+// printer.work = false 코드 개선 (interrupt 이용)
+public class MyPrinter3 {
+
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        Thread printerThread = new Thread(printer, "printer");
+        printerThread.start();
+
+        Scanner userInput = new Scanner(System.in);
+        while (true) {
+            log("프린터할 문서를 입력하세요. 종료 (q): ");
+            String input = userInput.nextLine();
+            if (input.equals("q")) {
+                printerThread.interrupt();
+                break;
+            }
+            printer.addJob(input);
+        }
+
+    }
+
+    static class Printer implements Runnable {
+        volatile boolean work = true;
+        Queue<String> jobQueue = new ConcurrentLinkedQueue<>();
+
+        @Override
+        public void run() {
+            while (!Thread.interrupted()) { // interrupted 상태 확인 후 바로 변경
+                if (jobQueue.isEmpty()) {
+                    continue;
+                }
+                try {
+                    String job = jobQueue.poll();
+                    log("출력 시작: " + job + ", 대기 문서: " + jobQueue);
+                    Thread.sleep(3000);
+                    log("출력 완료");
+                } catch (InterruptedException e) {
+                    log("인터럽트!");
+                    break;
+                }
+            }
+            log("프린터 종료");
+        }
+
+        public void addJob(String input) {
+            jobQueue.offer(input);
+        }
+    }
+}
